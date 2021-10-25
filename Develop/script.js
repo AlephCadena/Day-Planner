5dc8bdde19220465db67339e40d6c1454b13a5b7
Aleph Cadena <alephcadena@gmail.com>
2021-10-25 00:09:19 -0500

    updated

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,39 @@
 var date = moment().format( 'dddd MMM Do, YYYY' );
 $('#currentDay').text(date);
 
-var storedEvent = JSON.parse(localStorage.getItem('storedEvent')) || [];
-var indexTime;
-var eventBlock;
+var storedEvent = JSON.parse(localStorage.getItem('storedEvent'));
+var actTime;
+var stopEvent;
 var time;
 
 function init() {
 
-    timeColor();
-
     for ( var i=9; i<=17; i++) {
 
-        indexTime = '#' + [i];
+        actTime = '#' + [i];
     
-        $(indexTime).on('click', function(event) {
-          if (event.target.className == 'material-icons unsaved') {
-            event.target.className = 'material-icons saved';
-            eventBlock =  $(event.target).parent().siblings().children().val();
+        $(actTime).on('click', function(event) {
+          if (event.target.any == 'material-icons unsaved') {
+            event.target.any = 'material-icons saved';
+            stopEvent =  $(event.target).parent().siblings().children().val();
             time = $(event.target).parent().parent().attr('id');
-            localStorage.setItem(time, eventBlock);
-          } else if (event.target.className == 'material-icons saved') {
-            event.target.className = 'material-icons unsaved';
-            eventBlock =  $(event.target).parent().siblings().children().val();
+            localStorage.setItem(time, stopEvent);
+          } 
+          else if (event.target.any == 'material-icons saved') {
+            event.target.any = 'material-icons unsaved';
+            stopEvent =  $(event.target).parent().siblings().children().val();
             time = $(event.target).parent().parent().attr('id');
             localStorage.removeItem(time);
           };
         });
 
-        $(indexTime).children().siblings().eq(2).children('.description').val(localStorage.getItem(i));
+        $(actTime).children().siblings().eq(2).children('.description').val(localStorage.getItem(i));
         
         if (localStorage.getItem(i) != null) {
 
-            $(indexTime).children().siblings().eq(3).children(i).removeClass('material-icons unsaved');
-            $(indexTime).children().siblings().eq(3).children(i).addClass('material-icons saved');
-            console.log($(indexTime).children().siblings().eq(3).children(i));
+            $(actTime).children().siblings().eq(3).children(i).removeClass('material-icons unsaved');
+            $(actTime).children().siblings().eq(3).children(i).addClass('material-icons saved');
+            console.log($(actTime).children().siblings().eq(3).children(i));
         };
     };
 
